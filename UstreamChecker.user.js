ce0d0c6e317781c0c8368f0de40d98d55245ec97
wonka <wonka.777@gmail.com>
2017-10-11 18:22:10 +0900

    0.1.8.1

--- UstreamChecker.user.js
+++ UstreamChecker.user.js
@@ -9,7 +9,7 @@
 // @grant       GM_setValue
 // @grant       GM_addStyle
 // @run-at      document-idle
-// @version     0.1.7.d
+// @version     0.1.8.d
 // ==/UserScript==
 
 /////////////////////////////
